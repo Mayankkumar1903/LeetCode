e53a6ad054d85559519ccb696ee3a1b1fcb8da58
Mayank Kumar <115803475+Mayankkumar1903@users.noreply.github.com>
2022-10-24 22:24:22 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (84.15%) - LeetHub

---
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    char repeatedCharacter(string s) {
+        unordered_map<int,int> mp;
+        char ans;
+        for(auto it:s){
+            if(mp.find(it)!=mp.end()){
+                ans=it;
+                break;
+            }
+            mp[it]++;
+        }
+        return ans;
+    }
+};
