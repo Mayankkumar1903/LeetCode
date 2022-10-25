6d90d14f452259b199ea29e3ef62daa68a970088
Mayank Kumar <115803475+Mayankkumar1903@users.noreply.github.com>
2022-10-26 01:26:59 +0530

    Time: 130 ms (23.62%), Space: 22.4 MB (5.20%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.cpp
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -1,61 +1,85 @@
 class Solution {
 public:
-   int largestArea(vector<int>& histogram){
-        int n=histogram.size(), area=0;
-        stack<int> s;
-        
-        for(int i=0; i<n; i++){
-            while(!s.empty() && histogram[s.top()]>=histogram[i]){
-                int top = s.top();
-                s.pop();
-                
-                int start;
-                if(s.empty())
-                    start = -1;
-                else
-                    start = s.top();
-                    
-                
-                int curr_area = histogram[top] * (i - start -1);
-                area = max(area, curr_area);
+    vector<int> NSL(vector<int> heights){ // Function to find indices of next smallest left element
+        vector<int> left;
+        stack<pair<int,int>> st;
+        for(int i=0;i<heights.size();i++){
+            if(st.empty())
+                left.push_back(-1);
+            else if(!st.empty() && st.top().first<heights[i])
+                left.push_back(st.top().second);
+            else if(!st.empty() && st.top().first>=heights[i]){
+                while(!st.empty() && st.top().first>=heights[i])
+                    st.pop();
+                if(st.empty())
+                    left.push_back(-1);
+                else 
+                    left.push_back(st.top().second);
             }
-            s.push(i);
+            st.push({heights[i],i});
         }
-        
-        while(!s.empty()){
-            int top = s.top();
-            s.pop();
-
-            int start;
-            if(s.empty())
-                start = -1;
-            else
-                start = s.top();
-            
-            int curr_area = histogram[top] * (n - start -1);
-            area = max(area, curr_area);
-        }
-        
-        return area;
+        return left;
     }
-    
-    int maximalRectangle(vector<vector<char>>& matrix) {
-        int m=matrix.size();
-        if(m==0) return 0;
-        int n=matrix[0].size(), result=0;
-        vector<int> histogram(n, 0);
-        
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                if(matrix[i][j]=='1')
-                    histogram[j]+=1;
-                else
-                    histogram[j]=0;
+    vector<int> NSR(vector<int> heights){  // Function to find indices of next smallest right element
+        vector<int> right;
+        stack<pair<int,int>> st;
+        for(int i=heights.size()-1;i>=0;i--){
+            if(st.empty())
+                right.push_back(heights.size());
+            else if(!st.empty() && st.top().first<heights[i])
+                right.push_back(st.top().second);
+            else if(!st.empty() && st.top().first>=heights[i]){
+                while(!st.empty() && st.top().first>=heights[i])
+                    st.pop();
+                if(st.empty())
+                    right.push_back(heights.size());
+                else 
+                    right.push_back(st.top().second);
             }
-            
-            result = max(result, largestArea(histogram));
-            cout<<result<<" ";
+            st.push({heights[i],i});
         }
-        return result;
+        reverse(right.begin(),right.end());
+        return right;
+    }
+    int MAH(vector<int>& heights) {  //Function to find maximum area of histogram
+        vector<int> right;
+        vector<int> left;
+        
+        right=NSR(heights);
+        left=NSL(heights);
+        
+        vector<int> width;
+        int mx=0;
+        for(int i=0;i<left.size();i++){
+            width.push_back(right[i]-left[i]-1);
+        }
+        
+        for(int i=0;i<heights.size();i++){
+            mx=max(mx,heights[i]*width[i]);
+        }
+        
+        return mx;
+    }
+    int maximalRectangle(vector<vector<char>>& matrix) {  // Finally......our required Maximal Rectangle Function
+        int n=matrix.size();
+        if(n == 0){
+            return 0;
+        }
+        int m=matrix[0].size();
+        vector<int> v;
+        for(int j=0;j<m;j++){
+            v.push_back(matrix[0][j]-'0');
+        }
+        int mx= MAH(v);
+        for(int i=1;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(matrix[i][j]=='0')
+                    v[j]=0;
+                else
+                    v[j]=v[j]+(matrix[i][j]-'0');
+            }
+            mx=max(mx,MAH(v));
+        }
+        return mx;
     }
 };
