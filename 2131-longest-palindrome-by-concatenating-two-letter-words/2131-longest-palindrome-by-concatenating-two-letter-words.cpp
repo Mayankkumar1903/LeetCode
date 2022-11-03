152d25d9dc1459305013485b8d8e5fbec371ae76
Mayank Kumar <115803475+Mayankkumar1903@users.noreply.github.com>
2022-11-03 20:19:18 +0530

    Time: 958 ms (33.55%), Space: 168 MB (60.00%) - LeetHub

--- 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
@@ -1,28 +1,25 @@
 class Solution {
 public:
     int longestPalindrome(vector<string>& words) {
-        int res = 0;
-       unordered_map<string,int> hm; 
-       
-       for(string s:words){ // match to make pairs that can be taken in palindrome
-           string p = s;
-           reverse(p.begin(),p.end());
-           if(hm[p]>0){ // reverse of current string found, these 2 strings can be added to palindrome 
-               res+= 4;
-               hm[p]--;  // remove occurence of string as we used it for palindrome
-           }
-           else{
-               hm[s]++;   // reverse not fount , put it in hashmap
-           }
-       }
-         //***** to take element at the center with both letters same
-       for(auto i:hm){   // only strings with frequency 1 are left now  
-           if(i.first[0]==i.first[1] && i.second>0){ // to place in center of palindrome we needs chars to be same
-               return res+2;   
-           }
-       }
-
-       return res;
+       int length=0;
+        unordered_map<string, int> hm;
+        for(string s: words){
+            string check=s;
+            reverse(check.begin(),check.end());
+            if(hm[check] > 0){
+                length+=4;
+                hm[check]--;
+            }
+            else {
+                hm[s]++;
+            }
+        }
+        for(auto equal : hm){
+            if(equal.first[0]==equal.first[1] && equal.second >0)
+                return  length +=2;
+        }
+        
+        return length;
     }
     
 };
