cfdb8c1257a415bfa02753f189b0a8b59c593daf
Mayank Kumar <115803475+Mayankkumar1903@users.noreply.github.com>
2022-11-10 22:46:40 +0530

    Time: 11 ms (14.34%), Space: 8.6 MB (37.73%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     vector<int> runningSum(vector<int>& nums) {
-       vector<int>ans;
-        int sum = 0;
-        for(int i = 0; i < nums.size();i++)
-        {
-            sum = sum + nums[i];
-            ans.push_back(sum);
+        vector<int>ans;
+        ans.push_back(nums[0]);
+        for(int i=1;i<nums.size();i++){
+            ans.push_back(nums[i]+ans[i-1]);
+        
         }
         return ans;
     }
 };
-
