b26edc13e49fb67da3a829b66e21a3c8134ea09d
Mayank Kumar <115803475+Mayankkumar1903@users.noreply.github.com>
2022-11-13 19:22:17 +0530

    Time: 246 ms (59.64%), Space: 59.7 MB (66.45%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
-    int peakIndexInMountainArray(vector<int>& a) {
-       int n = a.size();
-        
-        //Peak exists somewhere in between start and end
-        int start = 1, end = n-2;
-        while(start<=end){
-            int mid = start + (end-start)/2;
-            if(a[mid] > a[mid-1] && a[mid] > a[mid+1]) return mid;
-            else if(a[mid-1] > a[mid]) end = mid-1;
-            else start = mid+1;
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int n= arr.size();
+        int start = 1;
+        int end = n-2;
+        while( start <= end){
+            int mid = start + (end - start)/2;
+            if(arr[mid] >arr[mid-1] && arr[mid]>arr[mid+1])
+                return mid;
+            else if ( arr[mid-1]>arr[mid])
+                end= mid-1;
+            else 
+                start = mid+1;
         }
         return -1;
     }
