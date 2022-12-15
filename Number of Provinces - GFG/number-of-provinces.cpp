34d4e32b171af61589874667f4af4a1fcb114da5
Mayank Kumar <115803475+Mayankkumar1903@users.noreply.github.com>
2022-12-16 01:43:23 +0530

    Added solution - LeetHub

--- Number of Provinces - GFG/number-of-provinces.cpp
+++ Number of Provinces - GFG/number-of-provinces.cpp
@@ -5,30 +5,107 @@ using namespace std;
 
 // } Driver Code Ends
 //User function Template for C++
+class DisjointSet
+
+{
+
+
+    // vector<int> parent, size;
+
+ 
+
+public:
+ vector<int> parent, size;
+    DisjointSet(int n)
+
+    {
+
+ 
+
+        parent.resize(n + 1);
+
+        size.resize(n + 1, 1);
+
+        for (int i = 0; i <= n; i++)
+
+        {
+
+            parent[i] = i;
+
+        }
+
+    }
+
+ 
+
+    int findUPar(int node)
+
+    {
+
+        if (node == parent[node])
+
+            return node;
+
+        return parent[node] = findUPar(parent[node]);
+
+    }
+
+ 
+
+    void unionBySize(int u, int v)
+
+    {
+
+        int ulp_u = findUPar(u);
+
+        int ulp_v = findUPar(v);
+
+        if (ulp_u == ulp_v)
+
+            return;
+
+        if (size[ulp_u] < size[ulp_v])
+
+        {
+
+            parent[ulp_u] = ulp_v;
+
+            size[ulp_v] += size[ulp_u];
+
+        }
+
+        else
+
+        {
+
+            parent[ulp_v] = ulp_u;
+
+            size[ulp_u] += size[ulp_v];
+
+        }
+
+    }
+
+};
 
 class Solution {
-public:
-	int n;
-	void dfs(vector<int>& vis, vector<vector<int>>& adj, int node) {
-		vis[node] = 1;
-		for (int i = 0; i < n; i++) {
-			if (adj[node][i] == 1 and vis[i] == -1) {
-				dfs(vis, adj, i);
-			}
-		}
-	}
-	int numProvinces(vector<vector<int>> adj, int V) {
-		n = adj.size();
-		vector<int> vis(n, -1);
-		int ans = 0;
-		for (int i = 0; i < n; i++) {
-			if (vis[i] == -1) {
-				++ans;
-				dfs(vis, adj, i);
-			}
-		}
-    return ans;
-	}
+  public:
+    int numProvinces(vector<vector<int>> adj, int V) {
+        // code here
+        DisjointSet ds(V);
+        for(int i=0;i<V;i++){
+            for(int j=0;j<V;j++){
+                if(adj[i][j]==1)
+                ds.unionBySize(i,j);
+            }
+        }
+        int count=0;
+        for(int i=0;i<V;i++){
+            if(ds.parent[i]==i)
+            count++;
+        }
+        return count;
+    }
 };
 
 //{ Driver Code Starts.
