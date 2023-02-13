6745e5fa52ec2f46cd88848a9f24375695861596
Mayank Kumar <115803475+Mayankkumar1903@users.noreply.github.com>
2023-02-13 19:11:32 +0530

    Added solution - LeetHub

--- Topological sort - GFG/topological-sort.cpp
+++ Topological sort - GFG/topological-sort.cpp
@@ -6,38 +6,29 @@ using namespace std;
 class Solution
 {
 	public:
+	void dfs(int node,int vis[],vector<int>adj[],stack<int>&st){
+	    vis[node]=1;
+	    for(auto it:adj[node]){
+	        if(!vis[it])dfs(it,vis,adj,st);
+	    }
+	    st.push(node);
+	}
 	//Function to return list containing vertices in Topological order. 
 	vector<int> topoSort(int V, vector<int> adj[]) 
 	{
 	    // code here
-	    int indegree[V]={0};
+	    int vis[V]={0};
+	    stack<int>st;
 	    for(int i=0;i<V;i++){
-	        for(auto it: adj[i]){
-	            indegree[it]++;
-	        }
+	        if(!vis[i])dfs(i,vis,adj,st);
 	    }
-	    
-	    queue<int>q;
-	    for(int i=0;i<V;i++){
-	        if(indegree[i]==0)
-	        q.push(i);
+	    vector<int> ans;
+	    while(!st.empty()){
+	        ans.push_back(st.top());
+	        st.pop();
 	    }
-	    
-	    vector<int> topo;
-	    while(!q.empty()){
-	        int node=q.front();
-	        q.pop();
-	        topo.push_back(node);
-	        
-	        for(auto it:adj[node]){
-	            indegree[it]--;
-	            if(indegree[it]==0)
-	            q.push(it);
-	        }
-	    }
-	        
-	        return topo;
-	    }
+	    return ans;
+	}
 };
 
 //{ Driver Code Starts.
