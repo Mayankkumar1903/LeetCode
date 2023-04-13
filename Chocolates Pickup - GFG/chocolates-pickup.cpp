eb12b630a531914e6568eba0b3157a5ff1db1cf5
Mayank Kumar <115803475+Mayankkumar1903@users.noreply.github.com>
2023-04-13 21:29:59 +0530

    Added solution - LeetHub

--- Chocolates Pickup - GFG/chocolates-pickup.cpp
+++ Chocolates Pickup - GFG/chocolates-pickup.cpp
@@ -6,31 +6,56 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-    int solve(int i,int j1,int j2, int n, int m, vector<vector<int>>& grid,vector<vector<vector<int>>> &dp){
-        if(j1<0 || j2<0 || j1>=m ||j2>=m)return -1e9;// if robots goes out of bound 
-        if(i==n-1){//if it reaches last row
-            if(j1==j2)//if both reach at same place in last row
-                return grid[i][j1];
-            else return grid[i][j1] + grid[i][j2];
-        }
-        if(dp[i][j1][j2] !=-1)return dp[i][j1][j2];
-        int maxi = INT_MIN;
-        //for one movement of robo1 there is 3 movement possible for robo 2 so there is 9 combo possible 
-        for(int di=-1;di<=+1;di++){
-            for(int dj=-1;dj<=1;dj++){
-                int ans = 0;
-                if(j1==j2)ans = grid[i][j1];
-                else ans = grid[i][j1] + grid[i][j2];
-                ans += solve(i+1,j1+di,j2+dj,n,m,grid,dp);
-                maxi = max(ans,maxi);
-            }
-        }
-        return dp[i][j1][j2]=maxi;
-    }
     int solve(int n, int m, vector<vector<int>>& grid) {
-        // we have to declare 3d DP bcz we have to store [row][col][col]
-        vector<vector<vector<int>>>dp(n,vector<vector<int>>(m,vector<int>(m,-1)));
-        return solve(0,0,m-1,n,m,grid,dp);
+        // code here
+        vector < vector < int >> front(m, vector < int > (m, 0)), cur(m, vector < int > 
+  (m, 0));
+
+  for (int j1 = 0; j1 < m; j1++) {
+    for (int j2 = 0; j2 < m; j2++) {
+      if (j1 == j2)
+        front[j1][j2] = grid[n - 1][j1];
+      else
+        front[j1][j2] = grid[n - 1][j1] + grid[n - 1][j2];
+    }
+  }
+
+  //Outer Nested Loops for travering DP Array
+  for (int i = n - 2; i >= 0; i--) {
+    for (int j1 = 0; j1 < m; j1++) {
+      for (int j2 = 0; j2 < m; j2++) {
+
+        int maxi = INT_MIN;
+
+        //Inner nested loops to try out 9 options
+        for (int di = -1; di <= 1; di++) {
+          for (int dj = -1; dj <= 1; dj++) {
+
+            int ans;
+
+            if (j1 == j2)
+              ans = grid[i][j1];
+            else
+              ans = grid[i][j1] + grid[i][j2];
+
+            if ((j1 + di < 0 || j1 + di >= m) ||
+              (j2 + dj < 0 || j2 + dj >= m))
+
+              ans += -1e9;
+            else
+              ans += front[j1 + di][j2 + dj];
+
+            maxi = max(ans, maxi);
+
+          }
+        }
+        cur[j1][j2] = maxi;
+      }
+    }
+    front = cur;
+  }
+
+  return front[0][m - 1];
         
     }
 };
